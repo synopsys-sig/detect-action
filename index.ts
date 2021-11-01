6413ce35fa83da2a08e64153cb887148de445f35
Richard Otte <rotte@blackducksoftware.com>
2021-11-01 16:46:03 -0400

    fix: Correcting module structure

--- index.ts
+++ index.ts
@@ -4,33 +4,35 @@ import fs from 'fs';
 import { spawnSync } from 'child_process';
 import * as uploadJson from './upload-json';
 
-let githubToken = core.getInput('github-token')
-let blackduckUrl = core.getInput('blackduck-url');
-let blackduckApiToken = core.getInput('blackduck-api-token');
-let outputPath = core.getInput('output-path');
+export function run() {
+  let githubToken = core.getInput('github-token')
+  let blackduckUrl = core.getInput('blackduck-url');
+  let blackduckApiToken = core.getInput('blackduck-api-token');
+  let outputPath = core.getInput('output-path');
 
-let context = github.context;
-let octokit = github.getOctokit(githubToken);
+  let context = github.context;
+  let octokit = github.getOctokit(githubToken);
 
-let detectArgs = `--blackduck.url=\"${ blackduckUrl }\" --blackduck.api.token=\"${ blackduckApiToken }\" --detect.blackduck.scan.mode=RAPID --detect.scan.output.path=\"${ outputPath }\"`
+  let detectArgs = `--blackduck.url=\"${ blackduckUrl }\" --blackduck.api.token=\"${ blackduckApiToken }\" --detect.blackduck.scan.mode=RAPID --detect.scan.output.path=\"${ outputPath }\"`
 
-if (process.platform === 'win32') {
-  spawnSync(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect7.ps1?$(Get-Random) | iex; detect ${detectArgs}"`);
-} else {
-  spawnSync(`bash <(curl -s -L https://detect.synopsys.com/detect7.sh) detect ${detectArgs}"`);
-}
+  if (process.platform === 'win32') {
+    spawnSync(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect7.ps1?$(Get-Random) | iex; detect ${detectArgs}"`);
+  } else {
+    spawnSync(`bash <(curl -s -L https://detect.synopsys.com/detect7.sh) detect ${detectArgs}"`);
+  }
 
-let scanJsonPaths = fs.readdirSync(outputPath);
+  let scanJsonPaths = fs.readdirSync(outputPath);
 
-uploadJson.run(outputPath, scanJsonPaths);
+  uploadJson.run(outputPath, scanJsonPaths);
 
-scanJsonPaths.forEach((jsonPath) => {
-  let scanJson = require(jsonPath);
+  scanJsonPaths.forEach((jsonPath) => {
+    let scanJson = require(jsonPath);
 
-  octokit.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: JSON.stringify(scanJson, undefined, 2)
+    octokit.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: JSON.stringify(scanJson, undefined, 2)
+    });
   });
-});
+}
