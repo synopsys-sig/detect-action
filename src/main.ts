b7cf9b4f90f3427495c38fa720f976c14e959264
Richard Otte <rotte@blackducksoftware.com>
2021-11-12 11:18:20 -0500

    feat: Automatically upload diagnostic zip if Detect is run in diagnostic mode via environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
 import {getInput} from '@actions/core'
+import * as glob from '@actions/glob'
 import fs from 'fs'
-import path from 'path'
-import {uploadJson} from './upload-json'
+import {uploadRapidScanJson, uploadDiagnosticZip} from './upload-artifacts'
 import {downloadAndRunDetect} from './detect-manager'
 import {commentOnPR} from './comment'
 
-export function run() {
+export async function run() {
   const githubToken = getInput('github-token')
   const blackduckUrl = getInput('blackduck-url')
   const blackduckApiToken = getInput('blackduck-api-token')
   const outputPath = getInput('output-path')
 
-  const detectArgs = `--blackduck.trust.cert=TRUE --blackduck.url="${blackduckUrl}" --blackduck.api.token="${blackduckApiToken}" --detect.blackduck.scan.mode=RAPID --detect.scan.output.path="${outputPath}"`
+  const detectArgs = `--blackduck.trust.cert=TRUE --blackduck.url="${blackduckUrl}" --blackduck.api.token="${blackduckApiToken}" --detect.blackduck.scan.mode=RAPID --detect.output.path="${outputPath}" --detect.scan.output.path="${outputPath}"`
 
   downloadAndRunDetect(detectArgs)
 
-  const scanJsonPaths = fs.readdirSync(outputPath).map(jsonPath => path.join(outputPath, jsonPath))
+  const jsonGlobber = await glob.create(`${outputPath}/*.json`)
+  const scanJsonPaths = await jsonGlobber.glob()
+  uploadRapidScanJson(outputPath, scanJsonPaths)
 
-  uploadJson(outputPath, scanJsonPaths)
+  const diagnosticMode = process.env.DETECT_DIAGNOSTIC?.toLowerCase() === 'true'
+  const extendedDiagnosticMode = process.env.DETECT_DIAGNOSTIC_EXTENDED?.toLowerCase() === 'true'
+  if (diagnosticMode || extendedDiagnosticMode) {
+    const diagnosticGlobber = await glob.create(`${outputPath}/runs/*.zip`)
+    const diagnosticZip = await diagnosticGlobber.glob()
+    uploadDiagnosticZip(outputPath, diagnosticZip)
+  }
 
   scanJsonPaths.forEach(jsonPath => {
     const rawdata = fs.readFileSync(jsonPath)
---
+++ src/upload-artifacts.ts
@@ -0,0 +1,28 @@
+import {warning, info} from '@actions/core'
+import {create, UploadOptions} from '@actions/artifact'
+
+export async function uploadRapidScanJson(outputPath: string, jsonFiles: string[]): Promise<void> {
+  uploadArtifact('Rapid Scan JSON', outputPath, jsonFiles)
+}
+
+export async function uploadDiagnosticZip(outputPath: string, diagnosticZips: string[]): Promise<void> {
+  uploadArtifact('Detect Diagnostic Zip', outputPath, diagnosticZips)
+}
+
+async function uploadArtifact(name: string, outputPath: string, files: string[]): Promise<void> {
+  const artifactClient = create()
+  const options: UploadOptions = {
+    continueOnError: false,
+    retentionDays: 0
+  }
+
+  const uploadResponse = await artifactClient.uploadArtifact(name, files, outputPath, options)
+
+  if (files.length === 0) {
+    warning(`Expected to upload ${name}, but the action couldn't find any. Was output-path set correctly?`)
+  } else if (uploadResponse.failedItems.length > 0) {
+    warning(`An error was encountered when uploading ${uploadResponse.artifactName}. There were ${uploadResponse.failedItems.length} items that failed to upload.`)
+  } else {
+    info(`Artifact ${uploadResponse.artifactName} has been successfully uploaded!`)
+  }
+}
