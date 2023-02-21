8d81fbc2481a2a2d1669ad7184b0a5efcd641ea9
Kiran Narayana <kirann@synopsys.com>
2023-02-21 14:50:57 +0530

    feat SIGINT-375: adding logs.

--- dist/index.js
+++ dist/index.js
@@ -438,7 +438,7 @@ function createComponentRow(component) {
     console.log('component:::::' + component);
     console.log('component.violatedPolicies::' + component.violatedPolicies);
     const violatedPolicies = component.violatedPolicies.join('<br/>');
-    console.log('violatedPolicies::' + violatedPolicies);
+    console.log('violatedPolicies:::' + violatedPolicies);
     const componentInViolation = (component === null || component === void 0 ? void 0 : component.href) ? `[${component.name}](${component.href})` : component.name;
     const componentLicenses = component.licenses.map(license => `${license.violatesPolicy ? ':x: &nbsp; ' : ''}[${license.name}](${license.href})`).join('<br/>');
     const vulnerabilities = component.vulnerabilities.map(vulnerability => `${vulnerability.violatesPolicy ? ':x: &nbsp; ' : ''}[${vulnerability.name}](${vulnerability.href})${vulnerability.cvssScore && vulnerability.severity ? ` ${vulnerability.severity}: CVSS ${vulnerability.cvssScore}` : ''}`).join('<br/>');
