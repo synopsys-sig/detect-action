0eb18599ecab19d73dc79a00bfdc597dc01b6c1f
Kiran Narayana <kirann@synopsys.com>
2023-02-22 13:13:04 +0530

    feat:added validation for violatedPolicies

--- src/detect/reporting.ts
+++ src/detect/reporting.ts
@@ -21,12 +21,16 @@ export async function createRapidScanReportString(policyViolations: IRapidScanRe
 }
 
 function createComponentRow(component: IComponentReport): string {
-  const violatedPolicies = component.violatedPolicies === undefined ? '' : component.violatedPolicies.join('<br/>')
+  const a = undefined
+  const violatedPolicies = component.violatedPolicies === undefined ? '[Not Required]' : component.violatedPolicies.join('<br/>')
   const componentInViolation = component?.href ? `[${component.name}](${component.href})` : component.name
-  const componentLicenses = component.licenses.map(license => `${license.violatesPolicy ? ':x: &nbsp; ' : ''}[${license.name}](${license.href})`).join('<br/>')
-  const vulnerabilities = component.vulnerabilities.map(vulnerability => `${vulnerability.violatesPolicy ? ':x: &nbsp; ' : ''}[${vulnerability.name}](${vulnerability.href})${vulnerability.cvssScore && vulnerability.severity ? ` ${vulnerability.severity}: CVSS ${vulnerability.cvssScore}` : ''}`).join('<br/>')
+  const componentLicenses = component.licenses.map(license => `${license.violatesPolicy ? ':x: &nbsp; ' : ''}[${a}](${a})`).join('<br/>')
+  console.log('componentLicenses::' + componentLicenses)
+  const vulnerabilities = component.vulnerabilities.map(vulnerability => `${vulnerability.violatesPolicy ? ':x: &nbsp; ' : ''}[${a.href}](${vulnerability.href})${vulnerability.cvssScore && vulnerability.severity ? ` ${vulnerability.severity}: CVSS ${vulnerability.cvssScore}` : ''}`).join('<br/>')
+  console.log('vulnerabilities::' + componentLicenses)
   const shortTermString = component.shortTermUpgrade ? `[${component.shortTermUpgrade.name}](${component.shortTermUpgrade.href}) (${component.shortTermUpgrade.vulnerabilityCount} known vulnerabilities)` : ''
   const longTermString = component.longTermUpgrade ? `[${component.longTermUpgrade.name}](${component.longTermUpgrade.href}) (${component.longTermUpgrade.vulnerabilityCount} known vulnerabilities)` : ''
 
   return `| ${violatedPolicies} | ${componentInViolation} | ${componentLicenses} | ${vulnerabilities} | ${shortTermString} | ${longTermString} |`
 }
+
