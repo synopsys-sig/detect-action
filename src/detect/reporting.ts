5ba98cfe668035d8882f400a786a1d053dc8b1a7
Kiran Narayana <kirann@synopsys.com>
2023-03-15 15:30:21 +0530

    feat:added validation for violatedPolicies

--- src/detect/reporting.ts
+++ src/detect/reporting.ts
@@ -22,6 +22,7 @@ export async function createRapidScanReportString(policyViolations: IRapidScanRe
 
 function createComponentRow(component: IComponentReport): string {
   const violatedPolicies = component.violatedPolicies.join('<br/>')
+  console.log('violatedPolicies' + violatedPolicies)
   const componentInViolation = component?.href ? `[${component.name}](${component.href})` : component.name
   const componentLicenses = component.licenses.map(license => `${license.violatesPolicy ? ':x: &nbsp; ' : ''}[${license.name}](${license.href})`).join('<br/>')
   const vulnerabilities = component.vulnerabilities.map(vulnerability => `${vulnerability.violatesPolicy ? ':x: &nbsp; ' : ''}[${vulnerability.name}](${vulnerability.href})${vulnerability.cvssScore && vulnerability.severity ? ` ${vulnerability.severity}: CVSS ${vulnerability.cvssScore}` : ''}`).join('<br/>')
